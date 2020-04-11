caa573b64990e431d8759fdc66ea21177bea15ea
chiazokam <chiazokamecheta@gmail.com>
2020-04-11 17:38:08 +0100

    undo gitignore of build folder

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-if(process.env.NODE_ENV === 'production'){
-    const buildPath = path.resolve(__dirname, './build');
-    const filePath = path.join(buildPath, 'index.html');
+const buildPath = path.resolve(__dirname, './build');
+const filePath = path.join(buildPath, 'index.html');
 
-    app.use(express.static(buildPath));
-    app.get('*', (req, res) => {
-        res.sendFile(filePath)
-    });
-}
+app.use(express.static(buildPath));
+app.get('*', (req, res) => {
+    res.sendFile(filePath)
+});
 
 const port = process.env.PORT || 8000;
 
